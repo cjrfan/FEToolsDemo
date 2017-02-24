76b298e5f476c47e586160c70ce3df6e970d53ea
cjrfan <2298579045@qq.com>
2017-02-24 16:42:49 +0800

    test gulp.task()
    test gulp.task()

--- Gulp/demo1/gulpfile.js
+++ Gulp/demo1/gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-	console.log('hello world');
+// gulp.task('default', function() {
+// 	console.log('hello world');
+// });
+
+gulp.task('one',function(){
+	// one 是一个异步执行任务
+	setTimeout(function(){
+		console.log('one is done');
+	},5000);
+});
+
+//two任务虽然依赖于one任务,但并不会等到one任务中的异步操作完成后再执行
+gulp.task('two',['one'],function(){
+	console.log('two is done');
 });
